837075cff804b954eb7227fb47e2e6a4df729e1e
Jovid <jovid@Jovids-MacBook-Pro.local>
2023-01-26 00:39:26 +0500

    init

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+import uvicorn
+
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "Merch"}
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host="localhost", port=8000, reload=True)
